317c4c51c03e0b4d6df24932df6d6792d124a297
junseo-yang <jsy724724@gmail.com>
2024-01-30 21:27:31 -0500

    Fix Responsive NavBar

--- components/Layout/NavBar/NavBar.tsx
+++ components/Layout/NavBar/NavBar.tsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import type { LinkProps } from "next/link";
 import { usePathname } from "next/navigation";
 import { NavLink } from "./NavLink";
 
 const navbarItems = [
+    { ref: "/about", label: "About" },
     { ref: "/book-now", label: "Book Now" },
+    { ref: "/contact", label: "Contact" },
     { ref: "/account/my-account", label: "Account", prefetch: false }
 ];
 
@@ -35,39 +37,67 @@ export function NavBar() {
     const [isMenuShown, setIsMenuShown] = useState(false);
     const pathname = usePathname();
     const [linkRef, setLinkRef] = useState<LinkProps["href"]>(pathname!);
+    const toggleOpen = useCallback(
+        () => setIsMenuShown(!isMenuShown),
+        [isMenuShown]
+    );
     return (
-        <nav
-            className={`${
-                isMenuShown
-                    ? "max-md:w-full max-md:opacity-100"
-                    : "max-md:w-0 max-md:opacity-0"
-            } max-md:animate-sideways-once right-0 top-0 z-40 overflow-hidden transition-all duration-500 ease-in-out max-md:absolute max-md:h-screen max-md:bg-white max-md:pt-24 md:block`}
-        >
-            <ul className="flex flex-col items-center justify-end gap-10 text-sm leading-[22px] md:flex-row md:gap-4 md:text-[15px] min-[900px]:gap-5 lg:gap-8">
-                {navbarItems.map(({ ref, label, prefetch }) => (
-                    <li key={ref} className="relative">
-                        <StyledNavLink
-                            isActive={ref === linkRef}
-                            href={ref}
-                            onClick={() => {
-                                setLinkRef(ref);
-                                setIsMenuShown(false);
-                            }}
-                            prefetch={prefetch}
-                        >
-                            {label}
-                        </StyledNavLink>
-                        <span className="absolute -bottom-5 left-[calc(50%_-_theme(space.24))] w-48 border-b-2 md:hidden" />
-                    </li>
-                ))}
-                <li className="order-first justify-end md:order-last">
-                    <div className="text-turquoise-200 flex flex-nowrap items-center justify-center gap-2">
-                        {/* <Login
+        <>
+            <button
+                type="button"
+                className="relative z-50 float-right block md:hidden"
+                onClick={toggleOpen}
+            >
+                <div className="absolute right-5 top-0 space-y-2">
+                    {(isMenuShown
+                        ? [
+                              "rotate-45 translate-y-[13px]",
+                              "opacity-0 h-0",
+                              "-rotate-45 translate-y-[-13px]"
+                          ]
+                        : ["", "", ""]
+                    ).map((className, index) => (
+                        <span
+                            // eslint-disable-next-line react/no-array-index-key
+                            key={index}
+                            className={`block h-[4px] w-8 transform bg-gray-600 transition duration-500 ease-in-out ${className}`}
+                        ></span>
+                    ))}
+                </div>
+            </button>
+            <nav
+                className={`${
+                    isMenuShown
+                        ? "max-md:w-full max-md:opacity-100"
+                        : "max-md:w-0 max-md:opacity-0"
+                } max-md:animate-sideways-once right-0 top-0 z-40 overflow-hidden transition-all duration-500 ease-in-out max-md:absolute max-md:h-screen max-md:bg-white max-md:pt-24 md:block`}
+            >
+                <ul className="flex flex-col items-center justify-end gap-10 text-sm leading-[22px] md:flex-row md:gap-4 md:text-[15px] min-[900px]:gap-5 lg:gap-8">
+                    {navbarItems.map(({ ref, label, prefetch }) => (
+                        <li key={ref} className="relative">
+                            <StyledNavLink
+                                isActive={ref === linkRef}
+                                href={ref}
+                                onClick={() => {
+                                    setLinkRef(ref);
+                                    setIsMenuShown(false);
+                                }}
+                                prefetch={prefetch}
+                            >
+                                {label}
+                            </StyledNavLink>
+                            <span className="absolute -bottom-5 left-[calc(50%_-_theme(space.24))] w-48 border-b-2 md:hidden" />
+                        </li>
+                    ))}
+                    <li className="order-first justify-end md:order-last">
+                        <div className="text-turquoise-200 flex flex-nowrap items-center justify-center gap-2">
+                            {/* <Login
                                 onActionClick={() => setIsMenuShown(false)}
                             /> */}
-                    </div>
-                </li>
-            </ul>
-        </nav>
+                        </div>
+                    </li>
+                </ul>
+            </nav>
+        </>
     );
 }
