c19f0c13c37720ab7ebbe7918af748916de6967f
junseo-yang <jsy724724@gmail.com>
2024-02-05 11:33:57 -0500

    Add Admin page

---
+++ app/admin/page.tsx
@@ -0,0 +1,67 @@
+import { getServerSession } from "next-auth";
+import Balancer from "react-wrap-balancer";
+import AuthOptions from "../api/auth/[...nextauth]/options";
+
+export default async function Page() {
+    const session = await getServerSession(AuthOptions);
+
+    // Admin Auth
+    if (session?.user.role !== "admin") {
+        return (
+            <h1
+                className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
+                style={{
+                    animationDelay: "0.15s",
+                    animationFillMode: "forwards"
+                }}
+            >
+                <p>
+                    <Balancer>Access Denied</Balancer>
+                </p>
+            </h1>
+        );
+    }
+
+    return (
+        <>
+            <h1
+                className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
+                style={{
+                    animationDelay: "0.15s",
+                    animationFillMode: "forwards"
+                }}
+            >
+                <p>
+                    <Balancer>Admin page</Balancer>
+                </p>
+            </h1>
+            <br />
+            <a
+                className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-2xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-3xl md:leading-[5rem]"
+                style={{
+                    animationDelay: "0.15s",
+                    animationFillMode: "forwards",
+                    cursor: "pointer"
+                }}
+                href="/admin/services"
+            >
+                <p>
+                    <Balancer>Go to Service Management</Balancer>
+                </p>
+            </a>
+            <a
+                className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-2xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-3xl md:leading-[5rem]"
+                style={{
+                    animationDelay: "0.15s",
+                    animationFillMode: "forwards",
+                    cursor: "pointer"
+                }}
+                href="/admin/teams"
+            >
+                <p>
+                    <Balancer>Go to Team Management</Balancer>
+                </p>
+            </a>
+        </>
+    );
+}
