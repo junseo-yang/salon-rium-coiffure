506977d2be7c5eeabc9be6be6a71e677f6c4b677
junseo-yang <jsy724724@gmail.com>
2024-02-08 11:00:54 -0500

    Update Metadata in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,21 +3,12 @@ import cx from "classnames";
 import Nav from "@/components/Layout/Nav";
 import Footer from "@/components/Layout/Footer";
 import { Suspense } from "react";
+import { Metadata } from "next";
 import { sfPro, inter } from "../fonts";
 
-export const metadata = {
-    title: "Precedent - Building blocks for your Next.js project",
-    description:
-        "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-    twitter: {
-        card: "summary_large_image",
-        title: "Precedent - Building blocks for your Next.js project",
-        description:
-            "Precedent is the all-in-one solution for your Next.js project. It includes a design system, authentication, analytics, and more.",
-        creator: "@steventey"
-    },
-    metadataBase: new URL("https://precedent.dev"),
-    themeColor: "#FFF"
+export const metadata: Metadata = {
+    title: "Salon Rium Coiffure",
+    description: "The Best Hair Salon in Montreal."
 };
 
 export default async function RootLayout({
@@ -36,7 +27,6 @@ export default async function RootLayout({
                     {children}
                 </main>
                 <Footer />
-                {/* <Analytics /> */}
             </body>
         </html>
     );
