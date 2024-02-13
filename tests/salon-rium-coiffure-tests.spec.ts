5db72c68b200dab8f3cbb8a9854127d87303b793
jisangkim <jkim4@conestogac.on.ca>
2024-02-13 18:02:50 -0500

    add test case for service

--- tests/salon-rium-coiffure-tests.spec.ts
+++ tests/salon-rium-coiffure-tests.spec.ts
@@ -1,4 +1,36 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Page } from "@playwright/test";
+
+const login = async (page: Page) => {
+    await page.goto("/");
+
+    // Click the Sign In button
+    await page.getByRole("button", { name: "Sign In" }).click();
+
+    // Click the Sign In with Google button
+    await page.getByText("Sign In with Google", { exact: true }).click();
+
+    // Expect a title "Sign in - Google Accounts" a substring.
+    await expect(page).toHaveTitle(/Sign in - Google Accounts/);
+
+    // Login with Google Email
+    await page
+        .getByLabel("Email or Phone")
+        .fill(process.env.USERNAME_P as string);
+
+    // Click Next
+    await page.locator("#identifierNext").click();
+
+    // Login with Google Password
+    await page
+        .getByLabel("Enter your password")
+        .fill(process.env.PASSWORD_P as string);
+
+    // Click Next
+    await page.locator("#passwordNext").click();
+
+    // Wait for Redirection
+    await page.waitForURL("http://localhost:3000/");
+}
 
 test("sign in with google", async ({ page }) => {
     await page.goto("/");
@@ -15,7 +47,7 @@ test("sign in with google", async ({ page }) => {
     // Login with Google Email
     await page
         .getByLabel("Email or Phone")
-        .fill(process.env.USERNAME as string);
+        .fill(process.env.USERNAME_P as string);
 
     // Click Next
     await page.locator("#identifierNext").click();
@@ -23,7 +55,7 @@ test("sign in with google", async ({ page }) => {
     // Login with Google Password
     await page
         .getByLabel("Enter your password")
-        .fill(process.env.PASSWORD as string);
+        .fill(process.env.PASSWORD_P as string);
 
     // Click Next
     await page.locator("#passwordNext").click();
@@ -35,10 +67,10 @@ test("sign in with google", async ({ page }) => {
     await expect(page).toHaveTitle(/Salon Rium Coiffure/);
 
     // Click the user icon to open the dropdown menu
-    await page.getByAltText(process.env.USERNAME as string).click();
+    await page.getByAltText(process.env.USERNAME_P as string).click();
 
     // Expect a username to be visible
-    await expect(page.getByText(process.env.USERNAME as string)).toBeVisible();
+    await expect(page.getByText(process.env.USERNAME_P as string)).toBeVisible();
 
     // Click the Logout button
     await page.getByText("Logout", { exact: true }).click();
@@ -46,3 +78,78 @@ test("sign in with google", async ({ page }) => {
     // Expect the Sign In button to be visible
     await expect(page.getByRole("button", { name: "Sign In" })).toBeVisible();
 });
+
+test("Get all service categories", async ({ page }) => {
+    await page.goto("/");
+
+    await expect(page.getByRole('heading', { name: 'Men', exact: true })).toBeVisible()
+    await expect(page.getByRole('heading', { name: 'Women', exact: true })).toBeVisible()
+    await expect(page.getByRole('heading', { name: 'Kid', exact: true })).toBeVisible()
+});
+
+test("Get services from seed data", async ({ page }) => {
+    await page.goto("/");
+
+    await expect(page.getByText("Men's Haircut - $45")).toBeVisible()
+    await expect(page.getByText("Men's Perm")).toBeVisible()
+    await expect(page.getByText("Women's Bangcut")).toBeVisible()
+});
+
+test("Blocked with no authentication", async ({ page }) => {
+    await page.goto("/admin/services");
+
+    await expect(page.getByText("Access Denied")).toBeVisible()
+});
+
+test("add service", async ({ page }) => {
+    await login(page)
+
+    await page.goto("/admin/services");
+
+    await page.locator('#btnAddService').click()
+
+    await page.locator('#name-input').fill('Test Service')
+    await page.locator('#price-input').fill('13.99')
+    await page.locator('#submit-button').click()
+
+    await page.waitForTimeout(1000);
+
+
+    await page.goto("/");
+    await expect(page.getByText("Test Service")).toBeVisible()
+});
+
+test("update service", async ({ page }) => {
+    await login(page)
+
+    await page.goto("/admin/services");
+
+    await page.locator('li').filter({ hasText: "Men's Perm" }).getByRole('button').first().click()
+    
+    await page.locator('#price-input').fill('99.99')
+    await page.locator('#submit-button').click()
+
+    await page.waitForTimeout(1000);
+
+
+    await page.goto("/");
+    await expect(page.getByText("99.99")).toBeVisible()
+});
+
+test("delete service", async ({ page }) => {
+    await login(page)
+
+    await page.goto("/admin/services");
+    page.on('dialog', dialog => {
+        dialog.accept()
+    })
+    await page.locator('li').filter({ hasText: "Women's Root Colour" }).getByRole('button').nth(1).click()
+    
+    await page.waitForTimeout(1000);
+
+
+    await page.goto("/");
+    await expect(page.getByText("Women’s Cut - $")).toBeHidden()
+});
+
+
