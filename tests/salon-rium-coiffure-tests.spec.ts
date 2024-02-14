114bc0bc012395da872aa1bfc50bff36585061a0
Yxu6894 <133616132+Yxu6894@users.noreply.github.com>
2024-02-13 22:06:35 -0500

    modified the staff tests

--- tests/salon-rium-coiffure-tests.spec.ts
+++ tests/salon-rium-coiffure-tests.spec.ts
@@ -15,7 +15,7 @@ const login = async (page: Page) => {
     // Login with Google Email
     await page
         .getByLabel("Email or Phone")
-        .fill(process.env.USERNAME_P as string);
+        .fill(process.env.GOOGLE_USERNAME as string);
 
     // Click Next
     await page.locator("#identifierNext").click();
@@ -23,15 +23,22 @@ const login = async (page: Page) => {
     // Login with Google Password
     await page
         .getByLabel("Enter your password")
-        .fill(process.env.PASSWORD_P as string);
+        .fill(process.env.GOOGLE_PASSWORD as string);
 
     // Click Next
     await page.locator("#passwordNext").click();
 
     // Wait for Redirection
     await page.waitForURL("http://localhost:3000/");
-}
+};
 
+const logout = async (page: Page) => {
+    // Click the user icon to open the dropdown menu
+    await page.getByAltText(process.env.GOOGLE_USERNAME as string).click();
+
+    // Click the Logout button
+    await page.getByText("Logout", { exact: true }).click();
+};
 
 test("sign in with google", async ({ page }) => {
     await page.goto("/");
@@ -48,7 +55,7 @@ test("sign in with google", async ({ page }) => {
     // Login with Google Email
     await page
         .getByLabel("Email or Phone")
-        .fill(process.env.USERNAME_P as string);
+        .fill(process.env.GOOGLE_USERNAME as string);
 
     // Click Next
     await page.locator("#identifierNext").click();
@@ -56,7 +63,7 @@ test("sign in with google", async ({ page }) => {
     // Login with Google Password
     await page
         .getByLabel("Enter your password")
-        .fill(process.env.PASSWORD_P as string);
+        .fill(process.env.GOOGLE_PASSWORD as string);
 
     // Click Next
     await page.locator("#passwordNext").click();
@@ -68,11 +75,11 @@ test("sign in with google", async ({ page }) => {
     await expect(page).toHaveTitle(/Salon Rium Coiffure/);
 
     // Click the user icon to open the dropdown menu
-    await page.getByAltText(process.env.USERNAME_P as string).click();
+    await page.getByAltText(process.env.GOOGLE_USERNAME as string).click();
 
     // Expect a username to be visible
     await expect(
-        page.getByText(process.env.USERNAME_P as string)
+        page.getByText(process.env.GOOGLE_USERNAME as string)
     ).toBeVisible();
 
     // Click the Logout button
@@ -82,58 +89,108 @@ test("sign in with google", async ({ page }) => {
     await expect(page.getByRole("button", { name: "Sign In" })).toBeVisible();
 });
 
-test("Get all staffs", async ({ page }) => {
+test("Get All Staffs", async ({ page }) => {
     await page.goto("/");
 
-    await expect(page.getByText('Our Staff')).toBeVisible()
+    await expect(page.getByText("Our Staff")).toBeVisible();
 });
 
-test("add staff", async ({ page }) => {
-    await login(page)
+test("Create Staffs", async ({ page }) => {
+    // Login
+    await login(page);
 
+    // Create a Staff
     await page.goto("/admin/staffs");
-
-    await page.locator('#btnAddStaff').click()
-
-    await page.locator('#name-input').fill('Test Staff')
-    await page.locator('#submit-button').click()
-
+    await page.locator("#btnAddStaff").click();
+    await page.locator("#name-input").fill("Test Staff");
+    await page.locator("#submit-button").click();
     await page.waitForTimeout(1000);
 
-
+    // Assert
     await page.goto("/");
-    await expect(page.getByText("Test Staff")).toBeVisible()
+    await expect(page.getByText("Test Staff")).toBeVisible();
+
+    // Cleanup: Delete the staff
+    await page.goto("/admin/staffs");
+    page.on("dialog", (dialog) => dialog.accept());
+    await page
+        .locator("li")
+        .filter({ hasText: "Test Staff" })
+        .getByRole("button")
+        .nth(1)
+        .click();
+    await page.waitForTimeout(1000);
+
+    // Logout
+    await logout(page);
 });
 
-test("update staff", async ({ page }) => {
-    await login(page)
+test("Update Staffs", async ({ page }) => {
+    // Login
+    await login(page);
 
+    // Create a staff to update
     await page.goto("/admin/staffs");
-
-    await page.locator('li').filter({ hasText: "Test Staff" }).getByRole('button').first().click()
-
-    await page.locator('#name-input').fill('John Doe')
-    await page.locator('#submit-button').click()
-
+    await page.locator("#btnAddStaff").click();
+    await page.locator("#name-input").fill("Test Staff");
+    await page.locator("#submit-button").click();
     await page.waitForTimeout(1000);
 
+    // Update the staff
+    await page
+        .locator("li")
+        .filter({ hasText: "Test Staff" })
+        .getByRole("button")
+        .first()
+        .click();
+    await page.locator("#name-input").fill("John Doe");
+    await page.locator("#submit-button").click();
+    await page.waitForTimeout(1000);
 
+    // Assert
     await page.goto("/");
-    await expect(page.getByText("John Doe")).toBeVisible()
+    await expect(page.getByText("John Doe")).toBeVisible();
+
+    // Cleanup: Delete the staff
+    await page.goto("/admin/staffs");
+    page.on("dialog", (dialog) => dialog.accept());
+    await page
+        .locator("li")
+        .filter({ hasText: "John Doe" })
+        .getByRole("button")
+        .nth(1)
+        .click();
+    await page.waitForTimeout(1000);
+
+    // Logout
+    await logout(page);
 });
 
-test("delete staff", async ({ page }) => {
-    await login(page)
+test("Delete Staffs", async ({ page }) => {
+    // Login
+    await login(page);
 
+    // Create a staff to delete
     await page.goto("/admin/staffs");
-    page.on('dialog', dialog => {
-        dialog.accept()
-    })
-    await page.locator('li').filter({ hasText: "John Doe" }).getByRole('button').nth(1).click()
-
+    await page.locator("#btnAddStaff").click();
+    await page.locator("#name-input").fill("Test Staff");
+    await page.locator("#submit-button").click();
     await page.waitForTimeout(1000);
 
+    // Delete the staff
+    page.on("dialog", (dialog) => dialog.accept());
+    await page
+        .locator("li")
+        .filter({ hasText: "Test Staff" })
+        .getByRole("button")
+        .nth(1)
+        .click();
+    await page.waitForTimeout(1000);
 
+    // Assert
     await page.goto("/");
-    await expect(page.getByText("John Doe")).toBeHidden()
+    await expect(page.getByText("Test Staff")).toBeHidden();
+
+    // Logout
+    await logout(page);
 });
