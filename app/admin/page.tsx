0973abde5a4b44525e4b7d79adc9153b48fe076f
Jisang Kim <jjoo0815@gmail.com>
2024-03-06 20:24:50 -0500

    add a link to appointment management

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -77,6 +77,19 @@ export default async function Page() {
                     <Balancer>Go to Booking Calendar</Balancer>
                 </p>
             </a>
+            <a
+                className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-2xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-3xl md:leading-[5rem]"
+                style={{
+                    animationDelay: "0.15s",
+                    animationFillMode: "forwards",
+                    cursor: "pointer"
+                }}
+                href="/admin/appointments"
+            >
+                <p>
+                    <Balancer>Go to Appointment Management</Balancer>
+                </p>
+            </a>
         </>
     );
 }
