dfcc277580521bfbc0b147d7535f7f3d2630bbf1
jisangkim <jkim4@conestogac.on.ca>
2024-03-15 16:05:30 -0400

    add prevent user to see blocked time

--- components/Booking/action.tsx
+++ components/Booking/action.tsx
@@ -108,6 +108,16 @@ export async function getAvailableTimes(
         }
     });
 
+    // get all blocking
+    const blockings = await prisma.blocking.findMany({
+        where: {
+            from_datetime: {
+                gte: targetDate,
+                lt: new Date(targetDate.getTime() + 24 * 60 * 60 * 1000)
+            }
+        }
+    });
+
     const availableTimes = times.filter((t) => {
         let isAvailable = true;
         const fromTime = moment(t.split(" ~ ")[0], "HH:mm");
@@ -122,6 +132,15 @@ export async function getAvailableTimes(
         appointments.forEach((a) => {
             const aStartTime = moment(a.from_date);
             const aEndTime = moment(a.to_date);
+
+            if (aStartTime.isBefore(toTime) && fromTime.isBefore(aEndTime)) {
+                isAvailable = false;
+            }
+        });
+
+        blockings.forEach((b) => {
+            const aStartTime = moment(b.from_datetime);
+            const aEndTime = moment(b.to_datetime);
 
             if (aStartTime.isBefore(toTime) && fromTime.isBefore(aEndTime)) {
                 isAvailable = false;
