a5eaa025c1cecba8d0d8d01cf7ee76a9c8d7ec7b
jisangkim <jkim4@conestogac.on.ca>
2024-03-14 21:19:04 -0400

    implement calendar view for blocking

--- components/Booking/AdminCalendar.tsx
+++ components/Booking/AdminCalendar.tsx
@@ -4,15 +4,36 @@
 
 import FullCalendar from "@fullcalendar/react";
 import timeGridPlugin from "@fullcalendar/timegrid";
-import { Appointment } from "@prisma/client";
+import { Appointment, Blocking } from "@prisma/client";
 import interactionPlugin from "@fullcalendar/interaction";
+import { EventInput } from "@fullcalendar/core";
 
-export default function AdminCalendar({ appointments }) {
-    const events = appointments.map((a: Appointment) => ({
-        title: `${a.customer_name} ${a.service_name} ${a.staff_name}`,
-        start: a.from_date,
-        end: a.to_date
-    }));
+export default function AdminCalendar({ appointments, blockings }) {
+    const events: EventInput = [];
+    events.push(
+        ...appointments.map((a: Appointment) => {
+            const r: EventInput = {
+                title: `${a.customer_name} ${a.service_name} ${a.staff_name}`,
+                start: a.from_date,
+                end: a.to_date
+            };
+
+            return r;
+        })
+    );
+
+    events.push(
+        ...blockings.map((b: Blocking) => {
+            const r: EventInput = {
+                title: `${b.name}`,
+                start: b.from_datetime,
+                end: b.to_datetime,
+                backgroundColor: "green"
+            };
+
+            return r;
+        })
+    );
 
     return (
         <>
