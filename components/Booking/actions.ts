4bbb09e812770295e8f7eeb9d17ddf32975574fd
Jisang Kim <jjoo0815@gmail.com>
2024-03-27 17:11:30 -0400

    fix on putting wrong duration

--- components/Booking/actions.ts
+++ components/Booking/actions.ts
@@ -64,7 +64,7 @@ export async function createAppointment(
 
             from_date: fromDatetime,
             to_date: toDatetime,
-            duration: "1hour",
+            duration,
 
             customer_name: customerName,
             customer_email: customerEmail,
