25d009defb25389f455ed756aabcf5020f0fa436
Jisang Kim <jjoo0815@gmail.com>
2024-03-27 20:50:04 -0400

    use actual duration of service instead of static 1 hour

--- components/Booking/actions.ts
+++ components/Booking/actions.ts
@@ -64,7 +64,7 @@ export async function createAppointment(
 
             from_date: fromDatetime,
             to_date: toDatetime,
-            duration,
+            duration: service.duration,
 
             customer_name: customerName,
             customer_email: customerEmail,
